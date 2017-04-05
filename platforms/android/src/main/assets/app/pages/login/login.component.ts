06377a4a7eb9fb4754ab18723b19bd862a4dbcb0
William Montiel <william.montiel@creardigital.com>
2017-04-05 09:25:48 -0500

    Angular 2 Route

--- platforms/android/src/main/assets/app/pages/login/login.component.ts
+++ platforms/android/src/main/assets/app/pages/login/login.component.ts
@@ -1,6 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { User } from "../../shared/user/user";
 import { UserService } from "../../shared/user/user.service";
+import { Router } from "@angular/router";
+import { Page } from "ui/page";
+import { Color } from "color";
+import { View } from "ui/core/view";
 
 @Component({
   selector: "my-app",
@@ -9,14 +13,20 @@ import { UserService } from "../../shared/user/user.service";
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   user: User;
   isLoggingIn = true;
+  @ViewChild("container") container: ElementRef;
 
-  constructor(private userService: UserService) {
+  constructor(private router: Router, private userService: UserService, private page: Page) {
     this.user = new User();
     this.user.email = "user@nativescript.org";
     this.user.password = "password";
+  }
+
+  ngOnInit() {
+    this.page.actionBarHidden = true;
+    this.page.backgroundImage = "res://bg_login";
   }
 
   submit() {
@@ -28,7 +38,11 @@ export class LoginComponent {
   }
 
   login() {
-    // TODO: Define
+    this.userService.login(this.user)
+      .subscribe(
+      () => this.router.navigate(["/list"]),
+      (error) => alert("Unfortunately we could not find your account.")
+      );
   }
 
   signUp() {
@@ -44,5 +58,10 @@ export class LoginComponent {
 
   toggleDisplay() {
     this.isLoggingIn = !this.isLoggingIn;
+    let container = <View>this.container.nativeElement;
+    container.animate({
+      backgroundColor: this.isLoggingIn ? new Color("white") : new Color("#301217"),
+      duration: 200
+    });
   }
 }
